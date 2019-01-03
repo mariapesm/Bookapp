336338c40500b8e2b1988c635d35ba0bf18eb230
mariapesm <youandmecanyousee@hotmail.com>
2019-01-03 19:34:04 +0200

    Update api.js

--- app/src/api.js
+++ app/src/api.js
@@ -1,14 +1,3 @@
-module.exports = {
-  entry: [
-    './app/index.html'
-  ],
-    output: {
-    path: __dirname,
-    publicPath: url:'https://bookappil.herokuapp.com',
-    filename: 'bundle.js'
-   
-  //url: 'http://localhost:9000',
-  books: {
-    url: 'https://www.googleapis.com/books/v1/volumes?q='
-  }
-}
+var msg = require('./Messages.js');
+
+console.log(msg);
