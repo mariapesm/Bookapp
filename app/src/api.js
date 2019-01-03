bdc0aba37795c375fe7a0be8cdad0a3a6aeda855
mariapesm <youandmecanyousee@hotmail.com>
2019-01-03 18:46:54 +0200

    Update api.js

--- app/src/api.js
+++ app/src/api.js
@@ -1,5 +1,12 @@
 module.exports = {
-  url: 'https://bookappil.herokuapp.com',
+  entry: [
+    './app/index.html'
+  ],
+    output: {
+    path: __dirname,
+    publicPath: url:'https://bookappil.herokuapp.com',
+    filename: 'bundle.js'
+   
   //url: 'http://localhost:9000',
   books: {
     url: 'https://www.googleapis.com/books/v1/volumes?q='
