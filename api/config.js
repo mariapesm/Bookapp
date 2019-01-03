af1cf66736cac0576adfb845bdfa9e44d066ad56
mariapesm <youandmecanyousee@hotmail.com>
2019-01-03 16:55:27 +0200

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: 9000,
   secret: 'minionsarecrazy',
-  database: 'mongodb://marype:book-trading-club@ds247944.mlab.com:47944/book-trading-club'
+  database: 'mongodb://marype:networkengineering12@ds247944.mlab.com:47944/book-trading-club'
   //database: 'mongodb:/47944/book-trading-club'
 }
