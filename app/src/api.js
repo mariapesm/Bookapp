a18c470eabb505907fb36a70ae30a7c9693b902a
mariapesm <youandmecanyousee@hotmail.com>
2019-01-03 17:38:16 +0200

    Update api.js

--- app/src/api.js
+++ app/src/api.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: 'https://bookappil.herokuapp.com',
+  url: 'https://mariapesm.github.io/Bookapp/docs',
   //url: 'http://localhost:9000',
   books: {
     url: 'https://www.googleapis.com/books/v1/volumes?q='
